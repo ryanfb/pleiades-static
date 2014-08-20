04c0690252f3156aadac1b9e06f75396bbcd6e91
Ryan Baumann <ryan.baumann@gmail.com>
2014-08-20 12:00:15 -0400

    Memoize HAML engine

--- pleiades-static.rb
+++ pleiades-static.rb
@@ -11,9 +11,9 @@ places_csv, names_csv, locations_csv = ARGV
 
 def render_place(place)
   template = IO.read(File.join('pleiades-static-place.haml'))
-  haml_engine = Haml::Engine.new(template, :format => :html5)
+  $haml_engine ||= Haml::Engine.new(template, :format => :html5)
   open("places/#{place['id']}.html",'w') {|file|
-    file.write(haml_engine.render(Object.new, :place => place))
+    file.write($haml_engine.render(Object.new, :place => place))
   }
 end
 
