67e6f71a6ec49f1550b9bee5764a15b26e0117c5
Ryan Baumann <ryan.baumann@gmail.com>
2014-08-20 11:27:52 -0400

    Add update script

---
+++ update.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget http://atlantides.org/downloads/pleiades/dumps/pleiades-places-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-locations-latest.csv.gz http://atlantides.org/downloads/pleiades/dumps/pleiades-names-latest.csv.gz
+gunzip *.csv.gz
+./pleiades-hierarchical.rb pleiades-places-latest.csv pleiades-names-latest.csv pleiades-locations-latest.csv
